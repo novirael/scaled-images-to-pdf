66bbfea734211c5544ae199509073c01644818a3
Andrii Alieksieiev <andrii.alieksieiev@stxnext.pl>
2023-09-01 22:32:56 +0200

    feat: Add error handling; Improve code structure

--- src/app.py
+++ src/app.py
@@ -1,59 +1,45 @@
 import os
-from flask import Flask, render_template, request, send_file
-from fpdf import FPDF
-from PIL import Image
+from pathlib import Path
+from typing import Optional
+from flask import Flask, Response, render_template, send_file, request
 from uuid import uuid4
+from pdf_generator import generate_pdf_from_path
+
+
 app = Flask(__name__)
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
-def get_orientation(width, height):
-    return "landscape" if width > height else "portrait"
-
-
-def generate_pdf_from_path(path):
-    # Get the list of images
-    image_list = os.listdir(path)
-
-    # Create a PDF object
-    pdf = FPDF(format='A4')
-
-    scale_ratio = 11.6
-
-    # Iterate over the images
-    for image in image_list:
-        image_file = os.path.join(path, image)
-
-        image_obj = Image.open(image_file)
-        w, h = image_obj.size
-        orientation = get_orientation(w, h)
-
-        pdf.add_page(orientation=orientation)
-        pdf.image(image_file, 0, 0, w / scale_ratio, h / scale_ratio, keep_aspect_ratio=True)
-
-    # Save the PDF file
-    pdf.output(f"{path}/output.pdf")
+APP_ROOT: Path = os.path.dirname(os.path.abspath(__file__))
 
 
 @app.route("/")
-def index():
+def index() -> Response:
     return render_template("upload.html")
 
 
 @app.route("/upload", methods=['POST'])
-def upload():
-    target = os.path.join(APP_ROOT, f"images/{uuid4()}")
+def upload() -> Response:
 
+    # Get the scale ratio from form
+    try:
+        scale_ratio: float = float(request.form.get('scale', 11.6))
+    except (TypeError, ValueError):
+        message = "The scale ratio should be float type"
+        return render_template('error.html', error_message=message)
+
+    # Get the file from form
+    target: Path = os.path.join(APP_ROOT, f"images/{uuid4()}")
     if not os.path.isdir(target):
-        os.mkdir(target)
+        os.makedirs(target)
 
     for file in request.files.getlist("file"):
-        filename = file.filename
-        destination = "/".join([target, filename])
-        file.save(destination)
+        filename: Optional[str] = file.filename
+        if filename:
+            destination: Path = "/".join([target, filename])
+            file.save(destination)
+        else:
+            message = "Have you uploaded the image ? Reload the page and try again"
+            return render_template('error.html', error_message=message)
 
-    generate_pdf_from_path(target)
+    generate_pdf_from_path(target, scale_ratio)
 
     return send_file(f"{target}/output.pdf", as_attachment=True)
 
---
+++ src/pdf_generator.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+from PIL import Image
+from fpdf import FPDF
+
+from data_types import Orientation
+
+
+def get_orientation(width: int, height: int) -> Orientation:
+    return Orientation.LANDSCAPE.value if width > height else Orientation.PORTRAIT.value
+
+
+def generate_pdf_from_path(path: Path, scale_ratio: float = 11.6) -> None:
+
+    # Get the list of images
+    image_list: list[Path] = os.listdir(path)
+
+    # Create a PDF object
+    pdf = FPDF(format='A4')
+
+    # Iterate over the images, add new page and image on it
+    for image in image_list:
+        image_file: Path = os.path.join(path, image)
+        image_obj = Image.open(image_file)
+        w, h = image_obj.size
+        orientation: Orientation = get_orientation(w, h)
+
+        pdf.add_page(orientation=orientation)
+        pdf.image(image_file, 0, 0, w / scale_ratio, h / scale_ratio, keep_aspect_ratio=True)
+
+    # Save the PDF file
+    pdf.output(f"{path}/output.pdf")
